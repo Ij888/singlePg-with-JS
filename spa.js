bf453f972f1f1cae37109707cfe1eb1d05dd3e9d
Ij888 <ij.ofili@gmail.com>
2015-01-08 04:28:36 +0000

    added entry

--- spa.js
+++ spa.js
@@ -1,4 +1,11 @@
-//views&controllers
+//
 
 var posts =
-    [  ];
+    {
+		title:"some title";, body:"...";, author:;, date:;, 
+		comments:[{name:;, email:;, comment:;}, {name:;, email:;, comment:;}, {}];,
+		tags:["tag1", "tag2", "tag3"];
+	
+	};
+	
+	authors = {_id:"man male";, password:"//hashed databyte";};
